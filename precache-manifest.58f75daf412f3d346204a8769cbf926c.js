181c51b6b21b933dcff4a448c51c66ab7194cae9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-12 02:41:35 +0000

    Deploy anmolvj/PingPong to github.com/anmolvj/PingPong.git:gh-pages

---
+++ precache-manifest.58f75daf412f3d346204a8769cbf926c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81ac0d9f26d48d0ec764e9ad1eb723d",
+    "url": "/PingPong/index.html"
+  },
+  {
+    "revision": "0a1257deb1af9d0d1c58",
+    "url": "/PingPong/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "2238cc13a05e44703189",
+    "url": "/PingPong/static/js/2.f9fb2358.chunk.js"
+  },
+  {
+    "revision": "0a1257deb1af9d0d1c58",
+    "url": "/PingPong/static/js/main.588eb18e.chunk.js"
+  },
+  {
+    "revision": "3ca6dea7dbac89df8849",
+    "url": "/PingPong/static/js/runtime-main.9af18176.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/PingPong/static/media/logo.25bf045c.svg"
+  }
+]);
